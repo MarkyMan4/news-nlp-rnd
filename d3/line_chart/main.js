8e335d3a9c71185fe2d6bea7488e38ca2eb66239
MarkyMan4 <markus.hutnik@gmail.com>
2021-09-18 13:04:37 -0500

    got basic line chart working

--- d3/line_chart/main.js
+++ d3/line_chart/main.js
@@ -1,14 +1,14 @@
 const dummyData = [
-    {x: '2021-01-01', y: 5},
-    {x: '2021-01-02', y: 5},
-    {x: '2021-01-03', y: 7},
-    {x: '2021-01-04', y: 14},
-    {x: '2021-01-05', y: 12},
-    {x: '2021-01-06', y: 21},
-    {x: '2021-01-07', y: 30},
-    {x: '2021-01-08', y: 25},
-    {x: '2021-01-09', y: 23},
-    {x: '2021-01-10', y: 45},
+    {x: 1, y: 5},
+    {x: 2, y: 5},
+    {x: 3, y: 7},
+    {x: 4, y: 14},
+    {x: 5, y: 12},
+    {x: 6, y: 21},
+    {x: 7, y: 30},
+    {x: 8, y: 25},
+    {x: 9, y: 23},
+    {x: 10, y: 45},
 ];
 
 const margin = {
@@ -24,19 +24,21 @@ const height = 400;
 const svg = d3.select('svg').classed('container', true);
 
 const xScale = d3.scaleTime()
-    .domain(d3.extent(dummyData, d => d.x))
+    .domain([0, d3.max(dummyData, d => d.x)])
     .range([0, width]);
 
 const yScale = d3.scaleLinear()
     .domain([0, d3.max(dummyData, d => d.y)])
     .range([height, 0]);
 
+const line = d3.line()
+    .x(d => xScale(d.x))
+    .y(d => yScale(d.y));
+
 svg
-    .selectAll('chartLines')
-    .data(dummyData)
-    .enter()
     .append('path')
-        .attr('fill', 'none')
-        .attr('stroke', 'black')
-        .attr('stroke-width', 1)
-        .attr('d', d3.line().x(d => xScale(1)).y(d => yScale(1)));
+    .datum(dummyData)
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('d', line);
