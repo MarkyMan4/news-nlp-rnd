2514693def6df5467e9f6918813617d3137abe8f
MarkyMan4 <markus.hutnik@gmail.com>
2021-09-18 14:47:49 -0500

    line chart displays multiple lines

--- d3/line_chart/main.js
+++ d3/line_chart/main.js
@@ -1,3 +1,7 @@
+/*
+ * Note: as a React component, this could take in an array of data sets and draw a line for each one
+ */ 
+
 const dummyData = [
     {x: 1, y: 5},
     {x: 2, y: 5},
@@ -9,6 +13,19 @@ const dummyData = [
     {x: 8, y: 25},
     {x: 9, y: 23},
     {x: 10, y: 45},
+];
+
+const dummyData2 = [
+    {x: 1, y: 45},
+    {x: 2, y: 30},
+    {x: 3, y: 20},
+    {x: 4, y: 5},
+    {x: 5, y: 14},
+    {x: 6, y: 10},
+    {x: 7, y: 7},
+    {x: 8, y: 5},
+    {x: 9, y: 3},
+    {x: 10, y: 5},
 ];
 
 
@@ -50,10 +67,20 @@ const line = d3.line()
     .x(d => xScale(d.x) + margin.left)
     .y(d => yScale(d.y) - margin.bottom);
 
+// line 1
 svg
     .append('path')
     .datum(dummyData)
     .attr('fill', 'none')
-    .attr('stroke', 'black')
+    .attr('stroke', 'red')
+    .attr('stroke-width', 1)
+    .attr('d', line);
+
+// line 2
+svg
+    .append('path')
+    .datum(dummyData2)
+    .attr('fill', 'none')
+    .attr('stroke', 'blue')
     .attr('stroke-width', 1)
     .attr('d', line);
